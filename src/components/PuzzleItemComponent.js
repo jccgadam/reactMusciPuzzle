fbb4b66e28bc07222c38a19be7b85d7d49c332d4
jccgadam <jccgadam@hotmail.com>
2020-06-21 20:55:01 -0700

    06/21/2020

--- src/components/PuzzleItemComponent.js
+++ src/components/PuzzleItemComponent.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import helpers from '../helpers'
 import { Button,Card } from 'antd';
+import createjs from 'createjs-module';
+import 'antd/es/card/style/index.css';
+import 'antd/es/button/style/index.css';
+import 'antd/es/cascader/style/index.css';
+const { Sound } = createjs;
 
 const PuzzleItemComponent = class extends React.Component{
     constructor(){
         super();
     }
 
+    createSoundItem = (id,setIsPlaying)=>{
+        let soundInstance = Sound.createInstance(id);
+        soundInstance.on('complete',()=>{
+            setIsPlaying(false);
+        })
+        return soundInstance;
+    }
+
     render(){
-        return <Card/>
+        const { props,createSoundItem } = this;
+        const { id,puzzleItem,setIsPlaying,playing } = props;
+        const soundItem = createSoundItem(id,setIsPlaying);
+        const play = ()=>{
+            setIsPlaying(true);
+            soundItem.play();
+        }
+        return <Card title={id+''}><button onClick={()=>play()} style={{ width: 300 }} disabled={playing}>play</button></Card>
     }
 }
 
--- src/containers/PuzzleItemsContainer.js
+++ src/containers/PuzzleItemsContainer.js
@@ -1,33 +1,91 @@
-import { Card,Button } from 'antd';
+import { Card,Button,Row,Col } from 'antd';
 import PuzzleItemComponent from '../components/PuzzleItemComponent';
 import React from 'react';
 import helpers from "../helpers";
 import _ from 'lodash';
+import {Queue} from "buckets-js";
+import createjs from 'createjs-module';
+import 'antd/es/row/style/css.js';
+import 'antd/es/col/style/css.js';
 
 const PuzzleItemsContainer  = class extends React.Component{
     constructor(){
         super();
         this.state = {
-            audioSprite:[]
+            audioSprite:[],
+            playing:false,
+            audioQueue: null
         }
     }
-    handleInit = ()=>{
+    handleInit = async  ()=>{
         const { init } = helpers;
-        const audioSprite = init();
+        const audioSprite = await init();
         this.setState({
             audioSprite
         })
     }
-    renderItems = ()=>{
+
+    parseItems = ()=>{
         const { state } = this;
         const { audioSprite } = state;
-        return _.map(audioSprite,i=><PuzzleItemComponent/>);
+        let tmpSound = null;
+        let header = null;
+        let myQueue = new Queue();
+        for(let i=0;i<audioSprite.length;i++){
+            let cur = audioSprite[i];
+            let { id } = cur;
+            try{
+                if(tmpSound!=null){
+                    tmpSound.on('complete',()=>{
+                        try{
+                            if(myQueue.peek()){
+                               myQueue.dequeue().play();
+                            }
+                        }catch(e){
+                            console.log(e);
+                        }
+                    })
+                }
+                tmpSound = createjs.Sound.createInstance(id);
+                if(i!='0'){
+                    myQueue.enqueue(tmpSound);
+                }
+                if(i==0){
+                    header = tmpSound;
+                }
+            }
+            catch(e){
+                console.log(e);
+            }
+        }
+    };
+
+    setIsPlaying = (playing)=>{
+        this.setState({
+            playing
+        })
+    }
+
+    renderItems = (audioSprite,len,setIsPlaying,playing)=>{
+        let span = Math.round(24/len);
+        return <Row>
+                { _.map(audioSprite,(item,i)=>
+                    <Col spa={span} key={i}>
+                        <PuzzleItemComponent id={i} key={i} puzzleItem={item}
+                                             setIsPlaying={setIsPlaying}
+                                             playing={playing}
+                        />
+                    </Col>)
+                }
+               </Row>
     }
     render(){
-        const { renderItems,handleInit } = this;
+        const { renderItems,handleInit,state,setIsPlaying } = this;
+        const { audioSprite,playing } = state;
+        const len = audioSprite.length;
         return <div>
-            { renderItems() }
-            <Button onClick={()=>handleInit}>Init</Button>
+            { renderItems(audioSprite,len,setIsPlaying,playing) }
+            <Button onClick={()=>handleInit()}>Init</Button>
         </div>
     }
 }
